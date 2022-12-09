ed85bf8271517539b20bf89ae74fb3df6f6c0bf6
Marcos Vinicius Mariano <marcos_viniciussilva@hotmail.com>
2022-12-09 18:06:48 -0300

    feature: create a router

---
+++ src/main/java/com/router/application/handler/RouterHandler.java
@@ -0,0 +1,27 @@
+package com.router.application.handler;
+
+import io.quarkus.vertx.web.RouteFilter;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.ext.web.RoutingContext;
+import com.router.service.TenantService;
+
+import javax.inject.Inject;
+
+public class RouterHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(RouterHandler.class);
+
+    @Inject
+    TenantService tenantService;
+
+    @RouteFilter
+    void myFilter(RoutingContext rc) throws Exception {
+        LOG.info("Path: "+rc.normalizedPath());
+        LOG.info("Header: "+rc.request().getHeader(tenantService.getTenantHeaderName()));
+
+        rc.redirect(tenantService.getTenantHost(rc.request().getHeader(tenantService.getTenantHeaderName())) + rc.request().uri())
+                .onSuccess(h-> LOG.info("Redirected with success"))
+                .onComplete(c->LOG.info("End Request"))
+                .onFailure(throwable -> new Exception("Error when redirect request"));
+    }
+}
